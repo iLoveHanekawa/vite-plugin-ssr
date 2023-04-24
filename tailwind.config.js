13e3e964c8a9ff0ec1ae4bba4bf3a1aa3bb34e9d
Arjun Tanwar <arjunthakur900@gmail.com>
2023-04-24 23:26:44 +0530

    project setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./renderer/**/*.{vue,js,ts,jsx,tsx}",
+    "./pages/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
