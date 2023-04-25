fd19015458c0601030d2859f2bcdc58a330995ce
Arjun Tanwar <arjunthakur900@gmail.com>
2023-04-25 10:09:48 +0530

    test

---
+++ .vercel/output/static/assets/pages/user/index.page.ab8f507d.js
@@ -0,0 +1,1 @@
+import{a as e,j as d}from"../../chunks/chunk-0e184ced.js";const r=({user:i})=>e("div",{children:[d("div",{children:i.id}),d("div",{children:i.name}),d("div",{children:i.email})]});export{r as Page};
--- server/index.ts
+++ server/index.ts
@@ -1,51 +1,51 @@
-// Note that this file isn't processed by Vite, see https://github.com/brillout/vite-plugin-ssr/issues/562
+// // Note that this file isn't processed by Vite, see https://github.com/brillout/vite-plugin-ssr/issues/562
 
-import express, {Request, Response} from 'express'
-import compression from 'compression'
-import { renderPage } from 'vite-plugin-ssr/server'
-import { root } from './root.js'
-const isProduction = process.env.NODE_ENV === 'production'
+// import express, {Request, Response} from 'express'
+// import compression from 'compression'
+// import { renderPage } from 'vite-plugin-ssr/server'
+// import { root } from './root.js'
+// const isProduction = process.env.NODE_ENV === 'production'
 
-startServer()
+// startServer()
 
-async function startServer() {
-  const app = express()
+// async function startServer() {
+//   const app = express()
 
-  app.use(compression())
+//   app.use(compression())
 
-  if (isProduction) {
-    const sirv = (await import('sirv')).default
-    app.use(sirv(`${root}/dist/client`))
-  } else {
-    const vite = await import('vite')
-    const viteDevMiddleware = (
-      await vite.createServer({
-        root,
-        server: { middlewareMode: true }
-      })
-    ).middlewares
-    app.use(viteDevMiddleware)
-  }
+//   if (isProduction) {
+//     const sirv = (await import('sirv')).default
+//     app.use(sirv(`${root}/dist/client`))
+//   } else {
+//     const vite = await import('vite')
+//     const viteDevMiddleware = (
+//       await vite.createServer({
+//         root,
+//         server: { middlewareMode: true }
+//       })
+//     ).middlewares
+//     app.use(viteDevMiddleware)
+//   }
 
-  app.get('/api/greet', (req: Request, res: Response) => {
-    return res.json({
-      greeting: "Hello Arjun"
-    })
-  })
+//   app.get('/api/greet', (req: Request, res: Response) => {
+//     return res.json({
+//       greeting: "Hello Arjun"
+//     })
+//   })
 
-  app.get('*', async (req, res, next) => {
-    const pageContextInit = {
-      urlOriginal: req.originalUrl
-    }
-    const pageContext = await renderPage(pageContextInit)
-    const { httpResponse } = pageContext
-    if (!httpResponse) return next()
-    const { body, statusCode, contentType, earlyHints } = httpResponse
-    if (res.writeEarlyHints) res.writeEarlyHints({ link: earlyHints.map((e) => e.earlyHintLink) })
-    res.status(statusCode).type(contentType).send(body)
-  })
+//   app.get('*', async (req, res, next) => {
+//     const pageContextInit = {
+//       urlOriginal: req.originalUrl
+//     }
+//     const pageContext = await renderPage(pageContextInit)
+//     const { httpResponse } = pageContext
+//     if (!httpResponse) return next()
+//     const { body, statusCode, contentType, earlyHints } = httpResponse
+//     if (res.writeEarlyHints) res.writeEarlyHints({ link: earlyHints.map((e) => e.earlyHintLink) })
+//     res.status(statusCode).type(contentType).send(body)
+//   })
 
-  const port = process.env.PORT || 3000
-  app.listen(port)
-  console.log(`Server running at http://localhost:${port}`)
-}
+//   const port = process.env.PORT || 3000
+//   app.listen(port)
+//   console.log(`Server running at http://localhost:${port}`)
+// }
