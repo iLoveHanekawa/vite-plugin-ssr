e141fd17deefd817ab286fb0b4edf7d714c82dfb
Arjun Tanwar <arjunthakur900@gmail.com>
2023-04-25 09:54:41 +0530

    plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import react from '@vitejs/plugin-react'
 import ssr from 'vite-plugin-ssr/plugin'
+import vercelSsr from '@magne4000/vite-plugin-vercel-ssr'
+import vercel from 'vite-plugin-vercel'
 import { UserConfig } from 'vite'
 
 const config: UserConfig = {
   plugins: [react(), ssr({
     prerender: true
-  })]
+  }), vercel(), vercelSsr()],
+  vercel: {
+  }
 }
 
 export default config
