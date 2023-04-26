f49ef81eec69e552bbef64b9291968e0c752cba9
Arjun Tanwar <arjunthakur900@gmail.com>
2023-04-26 10:09:36 +0530

    test4

--- pages/users/index.page.tsx
+++ pages/users/index.page.tsx
@@ -6,8 +6,20 @@ export type UserPageProps = {
     email: string
 }
 
-export const Page = ({ users }: { users: UserPageProps[] }) => {
+// export const Page = ({ users }: { users: UserPageProps[] }) => {
+//     return <ul>
+//         {users.map((props, index) => {
+//             return <li key = {index}>
+//                 <p>{props.id}</p>
+//                 <p>{props.name}</p>
+//                 <p>{props.email}</p>
+//             </li>
+//         })}
+//     </ul>
+// }
+
+export const Page = () => {
     return <ul>
-        hi
+        Hello
     </ul>
 }
