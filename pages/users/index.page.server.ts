1e4597a558c9b62ab460f9a560253ecee8399e33
Arjun Tanwar <arjunthakur900@gmail.com>
2023-04-26 10:06:18 +0530

    test2

--- pages/users/index.page.server.ts
+++ pages/users/index.page.server.ts
@@ -2,7 +2,7 @@ export const doNotPrerender = true;
 import { PageContext } from "../../renderer/types"
 import { UserPageProps } from "./index.page"
 
-export const onBeforeRender = async (pageContext: PageContext) => {
+export async function onBeforeRender(pageContext: PageContext) {
     const response = await fetch('https://jsonplaceholder.typicode.com/users')
     const users = await response.json() as UserPageProps[]
     return {
