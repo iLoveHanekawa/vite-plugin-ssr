7e6c7055e8a2844e31690f83b97d382f2061fcd7
Arjun Tanwar <arjunthakur900@gmail.com>
2023-04-26 09:43:49 +0530

    pending fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,13 @@ import ssr from 'vite-plugin-ssr/plugin';
 import vercel from 'vite-plugin-vercel';
 import vercelSsr from '@magne4000/vite-plugin-vercel-ssr';
 
-
 export default defineConfig(async ({ command, mode }) => {
   return {
-    plugins: [react(), ssr(), vercel(), vercelSsr()],
+    plugins: [react(), ssr({
+      prerender: {
+        partial: true
+      }
+    }), vercel(), vercelSsr()],
     vercel: {
     },
     server: {
