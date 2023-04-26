663ff600e803977c1b274e7fc7c5ada997894709
Arjun Tanwar <arjunthakur900@gmail.com>
2023-04-26 10:12:02 +0530

    asyn

--- pages/users/index.page.server.ts
+++ pages/users/index.page.server.ts
@@ -21,7 +21,7 @@ import { UserPageProps } from "./index.page"
 // }
 
 export async function onBeforeRender(pageContext: PageContext) {
-    // const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
     // const users = await response.json() as UserPageProps[]
     return {
         pageContext: {
