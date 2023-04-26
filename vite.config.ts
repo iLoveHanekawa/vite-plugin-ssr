07ac08e8c90005324197a04677faa25f6dfc9cbf
Arjun Tanwar <arjunthakur900@gmail.com>
2023-04-26 09:39:09 +0530

    last

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,14 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react'
 import ssr from 'vite-plugin-ssr/plugin';
 import vercel from 'vite-plugin-vercel';
 import vercelSsr from '@magne4000/vite-plugin-vercel-ssr';
 
+
 export default defineConfig(async ({ command, mode }) => {
   return {
-    plugins: [ssr({
-      prerender: {
-        partial: true
-      }
-    }), vercel(), vercelSsr()],
+    plugins: [react(), ssr(), vercel(), vercelSsr()],
     vercel: {
-      // Tweak what you need, check TS definition for details
     },
     server: {
       port: 3000
