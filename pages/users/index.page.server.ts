0d068b6833ed578e1cdcad46c68524dd60c2b6b9
Arjun Tanwar <arjunthakur900@gmail.com>
2023-04-26 10:11:15 +0530

    test5

--- pages/users/index.page.server.ts
+++ pages/users/index.page.server.ts
@@ -2,21 +2,31 @@ export const doNotPrerender = true;
 import { PageContext } from "../../renderer/types"
 import { UserPageProps } from "./index.page"
 
+// export async function onBeforeRender(pageContext: PageContext) {
+//     const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//     const users = await response.json() as UserPageProps[]
+//     return {
+//         pageContext: {
+//             pageProps: {
+//                 users: users.map((user, i) => {
+//                     return {
+//                         name: user.name,
+//                         email: user.email,
+//                         id: user.id
+//                     }
+//                 })
+//             }
+//         }
+//     }
+// }
+
 export async function onBeforeRender(pageContext: PageContext) {
-    const response = await fetch('https://jsonplaceholder.typicode.com/users')
-    const users = await response.json() as UserPageProps[]
+    // const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    // const users = await response.json() as UserPageProps[]
     return {
         pageContext: {
             pageProps: {
-                users: users.map((user, i) => {
-                    return {
-                        name: user.name,
-                        email: user.email,
-                        id: user.id
-                    }
-                })
             }
         }
     }
 }
-
